9999a50d52d434b77d570269d26f9cad14a3c21b
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-25 19:42:26 +0600

    connect with mongoDB

--- index.js
+++ index.js
@@ -4,48 +4,44 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
 app.use(cors());
 app.use(express.json());
 
+const uri =
+    'mongodb+srv://mongo_practice:xdO6HO2fY7bG6bFk@cluster0.q0gttvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-const uri = "mongodb+srv://mongo_practice:xdO6HO2fY7bG6bFk@cluster0.q0gttvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
 
-        const DB = client.db("Mongo_Practice");
-        const userCollection = DB.collection("users");
+        const DB = client.db('Mongo_Practice');
+        const userCollection = DB.collection('users');
 
+        app.get('/users', async (req, res) => {
 
-        
+            const user = await userCollection.find({}).toArray();
+            res.send(user);
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        })
+
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
     } finally {
-        // Ensures that the client will close when you finish/error
+        // Ensure to close the client when done
         // await client.close();
     }
 }
+
 run().catch(console.dir);
-
-
-app.use('/', (req, res) => {
-    res.send('running')
-})
 
 app.listen(port, () => {
     console.log(`Port ${port} is running`);
-})
+});
