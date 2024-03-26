ff569290e1e5f27b12ba5fa2afcf94d1172b748b
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-26 15:16:55 +0600

    add updated data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +25,11 @@ async function run() {
         const DB = client.db('Mongo_Practice');
         const userCollection = DB.collection('users');
 
+        app.get('/users', async (req, res) => {
+            const user = await userCollection.find({}).toArray();
+            res.send(user);
+        })
+
 
         app.post('/users', async (req, res) => {
 
@@ -34,6 +39,50 @@ async function run() {
 
         })
 
+        app.delete('/users/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+
+            const user = await userCollection.deleteOne(query);
+            console.log(user);
+            res.send(user);
+
+        })
+
+        app.get('/users/:id', async (req, res) => {
+
+            const id = req.body.id;
+            const query = { id: id };
+            const user = await userCollection.findOne(query);
+            console.log(user);
+            res.send(user);
+
+        })
+
+        app.put('/users/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const data = req.body;
+            const query = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+
+            const updatedData = {
+                $set: {
+                    name: data.name,
+                    email: data.email,
+                    like: data.like,
+                    dislike: data.dislike
+                }
+            }
+
+            const result = await userCollection.updateOne(query, updatedData, options);
+            console.log('updated', result);
+
+            res.send(result);
+        })
 
 
         await client.db('admin').command({ ping: 1 });
@@ -47,5 +96,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Port ${port} is running`);
+    console.log(`Server running on port ${port}`);
 });
