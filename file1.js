13751ff70f20bcaf866159e04acb4e820abd235b
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-08 16:04:03 +0600

    add function

--- file1.js
+++ file1.js
@@ -1,14 +1,6 @@
 
 
-const var1 = require('./file2');
 
-
-
-
-const add = var1.add(300, 500)
-
-const sub = var1.sub(300, 500)
-
-console.log(add, sub);
-
-
+((name) => {
+    console.log(`i will level up my ${name} skill`)
+})('javascript')
