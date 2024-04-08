47a36f4d7de4675406248c9e68f404ff3df7a5c5
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-08 15:46:56 +0600

    add import and export

---
+++ file1.js
@@ -0,0 +1,14 @@
+
+
+const var1 = require('./file2');
+
+
+
+
+const add = var1.add(300, 500)
+
+const sub = var1.sub(300, 500)
+
+console.log(add, sub);
+
+
---
+++ file2.js
@@ -0,0 +1,12 @@
+
+
+const add = (param1, param2) => {
+    return param1 + param2;
+}
+
+const sub = (param1, param2) => {
+    return param1 - param2;
+}
+
+
+module.exports = { add, sub }; 
