b92b3e48d805058788851e5c2c84b5b823a92b04
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-09 06:47:50 +0600

    add file handiling

--- Module2/events.js
+++ Module2/events.js
@@ -1,10 +1,10 @@
+
 const fs = require('fs');
+
 const path = require('path');
 
-// Use path.join to correctly concatenate directory and file name
 const filePath = path.join(__dirname, 'hello.txt');
 
-// Read file synchronously
 const file = fs.readFileSync(filePath, 'utf-8');
 
-console.log(file)
+console.log(file);
