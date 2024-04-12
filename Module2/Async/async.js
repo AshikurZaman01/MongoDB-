12d1089166a3bda6cebab162fb464e265c4e1092
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-12 16:34:44 +0600

    add async data

--- Module2/Async/async.js
+++ Module2/Async/async.js
@@ -4,15 +4,11 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'hello.txt');
 
-console.log('Before');
-
-const file = fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
 
     if (err) {
-        throw new Error('File not found');
+        throw new Error(err);
     }
     console.log(data);
 
 })
-
-console.log('After');
