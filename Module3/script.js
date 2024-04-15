036846acbf4e9169fbb6f87fc548898e3de8ad37
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-15 23:06:18 +0600

    add foreach output

---
+++ Module3/script.js
@@ -0,0 +1,10 @@
+// foreach , find , filter , map , indexOf , reduce
+
+const arr = [1, 2, 3, 4, 5];
+
+// if i want to add some text in each data. then have to use foreach loop
+
+// show output as data + " " + "hello"
+const dd = arr.forEach((data) => {
+    console.log(data + " " + "hello");
+})
