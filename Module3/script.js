277c6309b51133a3fb91dd9f5c6d8882d8cdc98a
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-15 23:30:27 +0600

    foreach skipping value

--- Module3/script.js
+++ Module3/script.js
@@ -4,7 +4,10 @@ const arr = [1, 2, 3, 4, 5];
 
 // if i want to add some text in each data. then have to use foreach loop
 
-// show output as data + " " + "hello"
-const dd = arr.forEach((data) => {
-    console.log(data + " " + "hello");
+// Skipping/Exiting Early:
+const dd = arr.forEach((ele) => {
+
+    if (ele === 2 || ele === 1) { return }
+    console.log(ele);
+
 })
