202016ffef49d77fdfb64a9ce3b25b1903d29621
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-15 21:43:25 +0600

    updated

--- Module2/Async/async.js
+++ Module2/Async/async.js
@@ -13,3 +13,19 @@ fs.readFile(filePath, 'utf-8', (err, data) => {
     console.log(data);
 
 })
+
+
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'hello.txt');
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+
+    if (err) {
+        throw new Error(err);
+    }
+    console.log(data);
+    console.log(data);
+
+})
