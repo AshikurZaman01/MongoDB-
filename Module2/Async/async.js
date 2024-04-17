3a9d8e8e2d5fab712f6972cf8e1bf7ad1239964f
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-17 23:22:25 +0600

    add synchronous

--- Module2/Async/async.js
+++ Module2/Async/async.js
@@ -1,1 +1,10 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const filepath = path.join(__dirname, 'hello.txt');
+
+// synchronous read
+const file = fs.readFileSync(filepath, 'utf-8');
+
+console.log(file);
