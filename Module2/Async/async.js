248fe0617a905ee4783ea0e35b1ca853527101b4
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-20 16:04:34 +0600

    add routing

--- Module2/Async/async.js
+++ Module2/Async/async.js
@@ -2,12 +2,17 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
 
-    res.end('Hello form server');
-
-
+    if (req.url === '/home' && req.method === 'GET') {
+        res.end('this is home data')
+    } else if (req.url === '/post' && req.method === 'GET') {
+        res.end('this is post data')
+    } else {
+        res.end('this is not valid url')
+    }
 })
 
 server.listen(3000, () => {
-    console.log('Server is running at port 3000');
+    console.log('server is running on port 3000');
 })
